a396b5fd63585363729864ea42042d3d6dc69647
DimaGashko <dgashko@mail.ua>
2019-01-25 09:09:35 +0200

    initSomeConfig

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const plugins = {
+   HtmlPlugin: require('html-webpack-plugin'),
+   CleanPlugin: require('clean-webpack-plugin'),
+}
+
+module.exports = {
+   entry: {
+      app: './src/index.ts',
+   },
+   output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: './',
+   },
+   module: {
+      rules: [{
+         test: /\.tsx?$/,
+         use: [
+            {
+               loader: 'ts-loader',
+               options: {
+                  transpileOnly: true,
+                  experimentalWatchApi: true,
+               },
+            },
+         ],
+         exclude: /node_modules/
+      }]
+   },
+   resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+   },
+   plugins: [
+      new plugins.CleanPlugin(['dist']),
+      new plugins.HtmlPlugin({
+         template: './src/index.pug',
+         hash: false,
+         minify: {
+            collapseWhitespace: true,
+         }
+      }),
+      new webpack.HashedModuleIdsPlugin(),
+   ],
+
+};
