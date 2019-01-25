744959ebd35be1e4c6ba0ca880675bcb0411ba0a
DimaGashko <dgashko@mail.ua>
2019-01-25 09:17:52 +0200

    testCode

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+class Hellower {
+   constructor(private name:string) {
+
+   }
+
+   public sayHello(): void {
+      console.log(`Hi, my name is ${this.name}`);
+   }
+}
+
+const hellower1 = new Hellower("Test1");
+const hellower2 = new Hellower("Test2");
+
+hellower1.sayHello();
+hellower2.sayHello();
