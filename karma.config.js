6f94b227605e5e123f5fbe80470267d3c6979892
DimaGashko <dgashko@mail.ua>
2019-01-28 21:38:43 +0200

    fixReporter

--- karma.config.js
+++ karma.config.js
@@ -4,20 +4,25 @@ module.exports = function (config) {
    config.set({
       basePath: '',
       frameworks: ['mocha', 'chai', 'sinon'],
-      reporters: ['spec', 'mocha'],
       files: ['test/*.ts'],
       exclude: [],
       preprocessors: {
          'test/**/*.ts': ['webpack'],
+         'src/**/*.ts': [],
       },
       webpack: {
          mode: 'development',
          module: webpackConfig.module,
          resolve: webpackConfig.resolve,
       },
+      coverageReporter: {
+         type: 'text',
+         file: 'output',
+      },
       mochaReporter: {
          showDiff: true,
       },
+      reporters: ['mocha'],
       port: 9876,
       colors: true,
       logLevel: config.LOG_INFO,
