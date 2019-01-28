4ce587f89ccf09578c3b6a2e2e29756d7566aff0
DimaGashko <dgashko@mail.ua>
2019-01-28 17:40:06 +0200

    firstLiveImgRender

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import './index.sass';
 import LiveImg from './components/LiveImg/LiveImg';
 
+const container = document.querySelector('.live-img');
+
 const liveImg = new LiveImg({
    width: 800,
    height: 600,
 });
+
+container.appendChild(liveImg.root);
  
 console.log(liveImg.width, liveImg.height);
