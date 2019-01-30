79c07ea9f9e7fb0f121757ce01cce77c38742263
DimaGashko <dgashko@mail.ua>
2019-01-30 20:00:43 +0200

    getAllObjectsTest

--- src/components/GameGrid/test/GameGrid.spec.ts
+++ src/components/GameGrid/test/GameGrid.spec.ts
@@ -28,7 +28,7 @@ describe('Class GameGrid', () => {
       const obj = new Obj(new Vector(50, 50));
 
       grid.add(obj);
-
+      
       obj.coords.x = 90;
       grid.add(obj);
 
@@ -36,6 +36,30 @@ describe('Class GameGrid', () => {
 
       expect(allObjects.length).equal(1);
       expect(allObjects[0]).equal(obj);
+   });
+
+   it('getAllObjects', () => {
+      const grid = new GameGrid(new Vector(100, 100), new Vector(10, 10));
+      const gridSize: Vector = (<any>grid).size;
+      const objects: Obj[] = [];
+
+      for (let i = 0; i < 15; i++) {
+         objects.push(new Obj(new Vector(
+            Math.random() * gridSize.x, Math.random() * gridSize.y
+         )));
+      }
+
+      objects.forEach(obj => grid.add(obj));
+
+      const allObjects = grid.getAllObjects();
+
+      const check1 = allObjects.every(obj => objects.indexOf(obj) != -1);
+      const check2 = objects.every(obj => allObjects.indexOf(obj) != -1);
+      const check3 = allObjects.length === objects.length;
+
+      expect(check1).equal(true);
+      expect(check2).equal(true);
+      expect(check3).equal(true);
    });
 
 }); 
