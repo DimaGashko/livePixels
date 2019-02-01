47f6d928cd0d0581e5b5d9c60fb3eaa527c6c88c
DimaGashko <dgashko@mail.ua>
2019-02-01 20:54:02 +0200

    initCanvasImgFill

---
+++ src/function/canvasImgFill.ts
@@ -0,0 +1,15 @@
+export default function canvasImgFill(
+   ctx: CanvasRenderingContext2D,
+   img: HTMLImageElement,
+   type: 'center' | 'cover'
+): void { 
+   
+}
+
+function drawCenter() { 
+
+}
+
+function drawCover() { 
+
+}
